a696db1a149335bc3128d776904c9cb5261cef3e
Gabriel <gabriel.padilha@brasilfone.com.br>
2023-03-27 17:20:53 +0000

    env testing

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -f "./src/@core/.env.testing" ]; then
+  cp ./src/@core/.env.testing.example ./src/@core/.env.testing
+fi
 
 npm install
 
--- src/@core/src/@seedwork/infra/testing/helpers/db.ts
+++ src/@core/src/@seedwork/infra/testing/helpers/db.ts
@@ -1,9 +1,11 @@
+import { configTest } from '#seedwork/infra/config';
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+import { Config } from 'sequelize/types';
 
 const sequelizeOptions: SequelizeOptions = {
-  dialect: 'sqlite',
-  host: ':memory:',
-  logging: false,
+  dialect: configTest.db.vendor,
+  host: configTest.db.host,
+  logging: configTest.db.logging,
 };
 
 export function setupSequelize(options: SequelizeOptions = {}) {
